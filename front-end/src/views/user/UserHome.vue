1c63eedb8aad8d115f7a72cc030d8f64b4eafa47
Wing_WR <wingwr998@gmail.com>
2025-07-21 03:40:59 +0800

    20250721: 完成用户的四个页面框架

--- front-end/src/views/user/UserHome.vue
+++ front-end/src/views/user/UserHome.vue
@@ -1,11 +1,11 @@
 <template>
-    <TopNav></TopNav>
-    <Intro></Intro>
-
-
+    <main class="pt-20 min-h-screen">
+        <TopNav />
+        <Intro />
+    </main>
 </template>
 
-<script lang = "ts" setup>
+<script lang="ts" setup>
 import TopNav from '@/components/user/TopNav.vue';
 import Intro from '@/components/user/Intro.vue';
 
