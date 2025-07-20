77bf0c85927a51fa71163bca990b05cb067a227c
Wing_WR <wingwr998@gmail.com>
2025-07-21 03:11:49 +0800

    20250721: 完成用户页面框架并加入路由

---
+++ front-end/src/views/user/UserHome.vue
@@ -0,0 +1,12 @@
+<template>
+    <TopNav></TopNav>
+    <Intro></Intro>
+
+
+</template>
+
+<script lang = "ts" setup>
+import TopNav from '@/components/user/TopNav.vue';
+import Intro from '@/components/user/Intro.vue';
+
+</script>
---
+++ front-end/src/views/user/UserRecommend.vue
@@ -0,0 +1,9 @@
+<template>
+    <TopNav></TopNav>
+</template>
+
+<script setup>
+import TopNav from '@/components/user/TopNav.vue';
+
+
+</script>
