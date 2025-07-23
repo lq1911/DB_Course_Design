16f1a9af48422fa498ae9a8db8aca5aec15ae984
陈柏熙 <2353120@tongji.edu.cn>
2025-07-23 14:06:26 +0800

    Update CreateUserDto.cs

--- back-end/Dtos/User/CreateUserDto.cs
+++ back-end/Dtos/User/CreateUserDto.cs
@@ -3,22 +3,22 @@ using System.ComponentModel.DataAnnotations;
 
 namespace BackEnd.Dtos.User
 {
-	// 用于新增用户时提交的数据
-	public class CreateUserDto
-	{
-		[Required]
-		[MaxLength(15)]
-		public string Username { get; set; } = null!;
+    // 鐢ㄤ簬鏂板鐢ㄦ埛鏃舵彁浜ょ殑鏁版嵁
+    public class CreateUserDto
+    {
+	[Required]
+	[MaxLength(15)]
+	public string Username { get; set; } = null!;
 
-		[Required]
-		[MaxLength(10)]
-		public string Password { get; set; } = null!;
+	[Required]
+	[MaxLength(10)]
+	public string Password { get; set; } = null!;
 
-		[Required]
-		public long PhoneNumber { get; set; }
+	[Required]
+	public long PhoneNumber { get; set; }
 
-		[Required]
-		[MaxLength(30)]
-		public string Email { get; set; } = null!;
-	}
+	[Required]
+	[MaxLength(30)]
+	public string Email { get; set; } = null!;
+    }
 }
