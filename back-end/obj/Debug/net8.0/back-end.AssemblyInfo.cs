348616403781b20d06937010a75742ea544dfcf5
L-Dramatic <3062059635@qq.com>
2025-07-23 19:20:42 +0800

    feat(Data): 完成前14个实体类的EF Core配置并移除obj目录

--- back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net8.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2e397f92561b21512d405c305b9534784e738f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ee4227398fcec1729b9420cb42beb9637df502")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
