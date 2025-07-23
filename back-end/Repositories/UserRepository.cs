90e547cda269164a19f2d77f090b4214bb26c303
陈柏熙 <2353120@tongji.edu.cn>
2025-07-23 14:11:44 +0800

    Update UserRepository.cs

--- back-end/Repositories/UserRepository.cs
+++ back-end/Repositories/UserRepository.cs
@@ -5,40 +5,40 @@ using Microsoft.EntityFrameworkCore;
 
 namespace BackEnd.Repositories
 {
-	public class UserRepository : IUserRepository
+    public class UserRepository : IUserRepository
+    {
+	private readonly AppDbContext _context;
+
+	// 通过构造函数注入数据库上下文
+	public UserRepository(AppDbContext context)
 	{
-		private readonly AppDbContext _context;
-
-		// 通过构造函数注入数据库上下文
-		public UserRepository(AppDbContext context)
-		{
-		    _context = context;
-		}
-
-		// 对数据进行操作
-		public async Task<IEnumerable<User>> GetAllAsync()
-		{
-		    return await _context.Users.ToListAsync();
-		}
-
-		public async Task<User?> GetByIdAsync(int id)
-		{
-		    return await _context.Users.FindAsync(id);
-		}
-
-		public async Task AddAsync(User user)
-		{
-		    await _context.Users.AddAsync(user);
-		}
-
-		public async Task DeleteAsync(User user)
-		{
-		    _context.Users.Remove(user);
-		}
-
-		public async Task SaveAsync()
-		{
-		    await _context.SaveChangesAsync();
-		}
+	    _context = context;
 	}
+
+	// 对数据进行操作
+	public async Task<IEnumerable<User>> GetAllAsync()
+	{
+	    return await _context.Users.ToListAsync();
+	}
+
+	public async Task<User?> GetByIdAsync(int id)
+	{
+	    return await _context.Users.FindAsync(id);
+	}
+
+	public async Task AddAsync(User user)
+	{
+            await _context.Users.AddAsync(user);
+	}
+
+	public async Task DeleteAsync(User user)
+	{
+	    _context.Users.Remove(user);
+	}
+
+	public async Task SaveAsync()
+	{
+	    await _context.SaveChangesAsync();
+	}
+    }
 }
