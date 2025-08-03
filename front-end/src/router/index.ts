e774dfea03c5129e0f2a009452ce6b93c759ebfe
Wing_WR <wingwr998@gmail.com>
2025-08-03 17:53:53 +0800

    20250803:  更新路由路径

--- front-end/src/router/index.ts
+++ front-end/src/router/index.ts
@@ -4,10 +4,6 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router' 
 import { getProjectName } from '@/stores/name'
 import LoginView from '@/views/login/LoginView.vue'
-import UserHomeView from '@/views/user/Homepage/UserHome.vue'
-import UserRecommendView from '@/views/user/Homepage/UserRecommend.vue'
-import UserRestaurantsView from '@/views/user/Homepage/UserRestaurants.vue'
-import UserOrderView from '@/views/user/Homepage/UserOrders.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -21,38 +17,6 @@ const routes: Array<RouteRecordRaw> = [
     component: LoginView,
     meta: {
       title: '登录'
-    }
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: UserHomeView,
-    meta: {
-      title: '首页'
-    }
-  },
-  {
-    path: '/recommend',
-    name: 'Recommend',
-    component: UserRecommendView,
-    meta: {
-      title: '推荐'
-    }
-  },
-  {
-    path: '/restaurants',
-    name: 'Restaurant',
-    component: UserRestaurantsView,
-    meta: {
-      title: '商家'
-    }
-  },
-  {
-    path: '/orders',
-    name: 'Order',
-    component: UserOrderView,
-    meta: {
-      title: '订单'
     }
   },
   {
