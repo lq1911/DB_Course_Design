7cd67e10c83f31a95307e29175a0b62ef4550a0a
Wing_WR <wingwr998@gmail.com>
2025-08-10 20:07:12 +0800

    20250810: 完成访问商家页面的基本框架

---
+++ front-end/src/views/user/StoreDetail/StoreLayout.vue
@@ -0,0 +1,29 @@
+<template>
+    <StoreNav :storeInfo="storeInfo" :storeID="storeID" />
+    <router-view />
+</template>
+
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed, ref, onMounted, watch } from 'vue'
+
+import type { StoreInfo } from '@/api/store_info'
+import { getStoreInfo } from '@/api/store_info'
+
+import StoreNav from '@/components/user/StoreDetail/StoreNav.vue';
+
+const route = useRoute();
+const storeID = computed(() => route.params.id as string);
+const storeInfo = ref<StoreInfo | null>(null);
+
+onMounted(async () => {
+    storeInfo.value = await getStoreInfo(storeID.value);
+})
+
+watch(storeID, async(newID, oldID) => {
+    if (newID != oldID) {
+        storeInfo.value = await getStoreInfo(storeID.value);
+    }
+})
+
+</script>
