c6bf4b66371b01817a04f2dc023d456f62acfd14
Wing_WR <wingwr998@gmail.com>
2025-08-11 00:57:37 +0800

    20250810: 完善菜单分类

--- front-end/src/components/user/StoreDetail/OrderView/MenuList.vue
+++ front-end/src/components/user/StoreDetail/OrderView/MenuList.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- 左侧商品分类导航 -->
-    <div class="w-1/5 bg-white rounded-lg shadow-sm" style="box-shadow: 2px 0 4px rgba(0,0,0,0.05)">
+    <div class="w-1/6 bg-white rounded-lg shadow-sm ml-[5%] mt-5 border-2" style="box-shadow: 2px 0 4px rgba(0,0,0,0.05)">
         <div class="py-4">
             <div v-for="(category, index) in categories" :key="category.id" @click="activeCategory = category.id"
                 :class="{
@@ -17,6 +17,28 @@
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
+const categories = [
+  { id: 1, name: "招牌推荐" },
+  { id: 2, name: "荤菜类" },
+  { id: 3, name: "素菜类" },
+  { id: 4, name: "丸子类" },
+  { id: 5, name: "豆制品" },
+  { id: 6, name: "主食类" },
+  { id: 7, name: "饮品" },
+];
+
+/* 实际代码
+const props = defineProps<{
+    activeCategory: number;
+}>();
+
+const activeCategory = props.activeCategory;
+*/
+
+// 测试使用，最后删除
+const activeCategory = ref(1);
 
 </script> 
 
