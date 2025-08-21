07770a06bad3fff353b2db5e3381262bd746a92b
MMmmsvt <1817705245@qq.com>
2025-08-21 10:30:28 +0800

    完成商家信息页

--- back-end/Program.cs
+++ back-end/Program.cs
@@ -36,12 +36,35 @@ builder.Services.AddCors(options =>
 });
 
 // 注册 Repository 层
+builder.Services.AddScoped<IAdministratorRepository, AdministratorRepository>();
+builder.Services.AddScoped<IAfterSaleApplicationRepository, AfterSaleApplicationRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
+builder.Services.AddScoped<ICourierRepository, CourierRepository>();
+builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
+builder.Services.AddScoped<IDeliveryComplaintRepository, DeliveryComplaintRepository>();
+builder.Services.AddScoped<IDeliveryTaskRepository, DeliveryTaskRepository>();
+builder.Services.AddScoped<IDishRepository, DishRepository>();
+builder.Services.AddScoped<IEvaluate_AfterSaleRepository, Evaluate_AfterSaleRepository>();
+builder.Services.AddScoped<IEvaluate_ComplaintRepository, Evaluate_ComplaintRepository>();
+builder.Services.AddScoped<IFavoriteItemRepository, FavoriteItemRepository>();
+builder.Services.AddScoped<IFavoritesFolderRepository, FavoritesFolderRepository>();
+builder.Services.AddScoped<IFoodOrderRepository, FoodOrderRepository>();
+builder.Services.AddScoped<IMenu_DishRepository, Menu_DishRepository>();
+builder.Services.AddScoped<IMenuRepository, MenuRepository>();
+builder.Services.AddScoped<IReview_CommentRepository, Review_CommentRepository>();
+builder.Services.AddScoped<ISellerRepository, SellerRepository>();
+builder.Services.AddScoped<IShoppingCartItemRepository, ShoppingCartItemRepository>();
+builder.Services.AddScoped<IShoppingCartRepository, ShoppingCartRepository>();
+builder.Services.AddScoped<IStoreRepository, StoreRepository>();
+builder.Services.AddScoped<IStoreViolationPenaltyRepository, StoreViolationPenaltyRepository>();
+builder.Services.AddScoped<ISupervise_Repository, Supervise_Repository>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
-builder.Services.AddScoped<IMerchantRepository, MerchantRepository>();
 
 // 注册 Service 层
 builder.Services.AddScoped<IUserService, UserService>();
 builder.Services.AddScoped<IMerchantService, MerchantService>();
+builder.Services.AddScoped<IMerchantInformationService, MerchantInformationService>();
 
 var app = builder.Build();
 
