6a5d84874c2d18fda8dc628947a3e729c7fd8a3d
Wing_WR <wingwr998@gmail.com>
2025-08-21 18:55:23 +0800

    20250821: 启用后端接口

--- front-end/src/api/user_home.ts
+++ front-end/src/api/user_home.ts
@@ -1,17 +1,23 @@
 import { getData } from '@/api/multiuse_function'
 
-export interface RecomStore {
+export interface showStore{
     id: number
+    image: string
     averageRating: number
     name: string
     monthlySales: number
 }
 
+export interface RecomStore {
+    recomStore: showStore[]
+}
+
+export interface AllStore{
+    allStore: showStore[]
+}
+
 export interface SearchStore {
-    averageRating: number
-    name: string
-    monthlySales: number
-    storeAddress: string
+    searchStore: showStore[]
 }
 
 export interface OrderInfo {
@@ -31,12 +37,16 @@ export interface UserInfo {
     image: string
 }
 
+export async function getAllStore() {
+    return getData<AllStore>(`//api/user/home/stores`);
+}
+
 export async function getRecomStore() {
-    return getData<RecomStore[]>(`/api/user/home/recommend`);
+    return getData<RecomStore>(`/api/user/home/recommend`);
 }
 
 export async function getSearchStore(UserID: number, Address: string, Keyword: string) {
-    return getData<SearchStore[]>(`/api/user/home/search`, {
+    return getData<SearchStore>(`/api/user/home/search`, {
         params: {
             userId: UserID,
             address: Address,
